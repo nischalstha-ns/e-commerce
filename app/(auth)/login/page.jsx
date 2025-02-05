b45408fb78f16e565290d5b2eeae7cfee096c7e7
nischalstha-ns <niscthastha123@gmail.com>
2025-02-05 23:58:54 +0545

    day 1 login runing

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,10 @@
+export default function Page(){
+    return (
+        <main className="w-full flex justify-center item-center bg-gray-300 p-24 min-h-screen">
+            <section className="flex flex-col gap-3">
+                <img src="/logo.jpg" alt="logo" />
+                <div className="bg-white p-10 "></div>
+            </section>
+        </main>
+    )
+}
---
+++ app/components/Header.jsx
@@ -0,0 +1,40 @@
+export default function Header(){
+    const menulist=[
+        {
+            name:"Home",
+            link:"/",
+        },
+        {
+            name:"About Us",
+            link:"/about-us",
+        },
+        {
+            name:"Contact Us",
+            link:"/contact-us",
+        },
+    ];
+    return (
+                
+              <nav className="py-3 px-14 border-b flex item-center justify-between">
+                <img className="h-10" src="/logo.jpg" alt="" />
+
+                <div className="flex gap-4 items-center font-semibold">
+  {menulist?.map((item) => {
+    return (
+      <a key={item?.link} href={item?.link} className="hover:text-blue-600">
+        <button>{item?.name}</button>
+      </a>
+    );
+  })}
+</div>
+
+<a href="/login">
+  <button className="bg-blue-600 px-3 py-3 rounded-full text-white">
+    Login
+  </button>
+</a>
+
+              </nav>
+            );
+          };
+          
