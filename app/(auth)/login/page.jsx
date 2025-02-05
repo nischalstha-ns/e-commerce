024ccf1aac55ae178f32a8292fa8ab4bb9419711
nischalstha-ns <niscthastha123@gmail.com>
2025-02-06 01:15:55 +0545

    working on login

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -2,9 +2,32 @@ export default function Page(){
     return (
         <main className="w-full flex justify-center item-center bg-gray-300 p-24 min-h-screen">
             <section className="flex flex-col gap-3">
+                <div>
                 <img src="/logo.jpg" alt="logo" />
-                <div className="bg-white p-10 "></div>
+                </div>
+                {/* <div className="bg-white p-10 rounded-x1 min-w-[440px]"></div> */}
+                <h1 className="font-bold text-x1">Login with Email</h1>
+                <form className="flex flex-col gap-3">
+                    
+                        <input 
+                        placeholder="Enter your Email" 
+                        type="email" 
+                        name="user-email" 
+                        id="user-email" 
+                        className="px-3 py-2 rounded border focus:outline-none w-full"
+                         />
+                         <input 
+                        placeholder="Enter your Password" 
+                        type="password" 
+                        name="user-password" 
+                        id="user-password" 
+                        className="px-3 py-2 rounded border focus:outline-none w-full"
+                         />
+                         <button>Login</button>
+                    
+                </form>
             </section>
         </main>
-    )
+    );
+
 }
--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import {HeroUIProvider} from '@heroui/react';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         <HeroUIProvider>
+          {children}
+         </HeroUIProvider>
+        
       </body>
     </html>
   );
