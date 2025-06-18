7ae1f818c1d2428f5a18f63a059278584dba8ab9
Nischal Shrestha <niscthastha123@gmail.com>
2025-06-18 23:10:52 +0545

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@heroui/react']
+  transpilePackages: ['@heroui/react'],
+  experimental: {
+    serverComponentsExternalPackages: ['stripe']
+  }
 };
 
 export default nextConfig;
