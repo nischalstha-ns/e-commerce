22ec3a22948ef5bd2e31dc776cfba1b429c29d0c
nischalstha-ns <niscthastha123@gmail.com>
2025-12-01 23:18:22 +0545

    up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@heroui/react'],
+  logging: {
+    fetches: {
+      fullUrl: false,
+    },
+  },
   images: {
     unoptimized: false,
     remotePatterns: [
